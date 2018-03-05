1569828eb76fc232bdd3567fb6de08d735b99ed7
Unknown <anl17149@uvg.edu.gt>
2018-03-05 12:09:12 -0600

    Modificaciones al factory para la implementación
    Se cambiaron las clases de la implementación del factory para los maps

--- HT6Estructuras/src/ht6estructuras/Factory.java
+++ HT6Estructuras/src/ht6estructuras/Factory.java
@@ -5,41 +5,36 @@
  */
 package ht6estructuras;
 
+import java.util.*;
+
 /**
  *
  * @author jose
  */
-public class Factory 
-{
-    
-    
-    Factory(){}
-    /**
-     * este es el metodo del diseño de patrones el cual nos devuelve la instancia
-     * del tipo de lista a usar dependiendo que ingresa el usuario
-     * @param opcion opcion elegida por el usuario
-     * @return instancia de lista a usar
-     */
-    public static Map getMap(String opcion) 
-    {
-        
-        if (opcion.equals("1")) 
-        {
-            return new StackArrayList();
-            
-        }
-        else if(opcion.equals("2"))
-        {
-            return new StackVector();
-        }
-        else if (opcion.equals("3"))
-        {
-            return new SinglyLinkedList();
-        }
-        
-        
-        
-    
-    
-    
+public class Factory {
+
+	Factory() {
+	}
+
+	/**
+	 * este es el metodo del diseño de patrones el cual nos devuelve la instancia
+	 * del tipo de lista a usar dependiendo que ingresa el usuario
+	 * 
+	 * @param opcion
+	 *            opcion elegida por el usuario
+	 * @return instancia de lista a usar
+	 */
+	public static Map getMap(String opcion) {
+
+		if (opcion.equals("1")) {
+			return new HashMap();
+
+		} else if (opcion.equals("2")) {
+			return new TreeMap();
+		} else if (opcion.equals("3")) {
+			return new LinkedHashMap();
+		}
+		return null;
+
+	}
 }
